cd51df27e2a3084e2c26350c3f5d379446e59cb6
Ross <rosshatokay@gmail.com>
2026-08-02 20:50:02 +0300

    asset

--- app/frontend/pages/Static/Index.tsx
+++ app/frontend/pages/Static/Index.tsx
@@ -44,8 +44,9 @@ export default function LandingPage() {
 						<p className="text-sm text-subtle">It takes only a minute.</p>
 					</div>
 					<div className="mt-30">
-						<video src="https://www.uselayo.com/videos/marketing-video.mp4" autoPlay muted loop className="rounded-xl"></video>
-						{/* <div className="rounded-xl w-full flex p-[4%_12%] pb-0 overflow-hidden justify-center sm:h-165" style={{ background: `url(https://images.unsplash.com/photo-1596367407372-96cb88503db6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D) center / cover` }}> */}
+						<div className="rounded-xl w-full flex md:p-[4%_12%] p-[4%_8%] !pb-0 overflow-hidden justify-center sm:h-165" style={{ background: `url(https://images.unsplash.com/photo-1596367407372-96cb88503db6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D) center / cover` }}>
+							<img src="/page.png" alt="" className="rounded-t-xl object-cover md:max-w-[800px] w-[700px]" />
+						</div>
 						{/* <div className="rounded-xl bg-card w-full flex p-[4%_12%] pb-0 overflow-hidden justify-center sm:h-165"></div> */}
 					</div>
 					{/* <div className="w-full mt-30">
